6caa2f9f060be78a2fa4ad09ea206b3c817778a6
Lucas Mallmann <lucasmallmann76@gmail.com>
2023-10-02 20:48:22 -0300

    chore: add sass loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import Cell from "@/components/Cell/Cell";
+
 function App() {
   return (
     <div>
       <h1>Ola mundo</h1>
+      <section>
+        <Cell />
+      </section>
     </div>
   );
 }
--- src/components/Cell/Cell.tsx
+++ src/components/Cell/Cell.tsx
@@ -1,7 +1,18 @@
+import { useState } from "react";
 import classes from "./Cell.module.scss";
 
 const Cell = () => {
-  return <div className={classes.cell}>oi</div>;
+  const [isEditMode] = useState(false);
+
+  if (isEditMode) {
+    return <input type="text" data-cell-id="1" className={classes.CellInput} />;
+  }
+
+  return (
+    <div className={classes.CellLabel} data-cell-id="1">
+      Value
+    </div>
+  );
 };
 
 export default Cell;
