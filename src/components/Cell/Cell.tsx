8cec3f7febd9821eb66386dc40fe96baa98d41cb
Lucas Mallmann <lucasmallmann76@gmail.com>
2023-10-02 21:23:54 -0300

    feat: create focusable cell input

--- src/components/Cell/Cell.tsx
+++ src/components/Cell/Cell.tsx
@@ -1,15 +1,37 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import classes from "./Cell.module.scss";
 
 const Cell = () => {
-  const [isEditMode] = useState(false);
+  const [isEditMode, setEditMode] = useState(false);
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const enableEditMode = () => {
+    setEditMode(true);
+    inputRef.current?.focus();
+  };
 
   if (isEditMode) {
-    return <input type="text" data-cell-id="1" className={classes.CellInput} />;
+    return (
+      <input
+        type="text"
+        data-cell-id="1"
+        className={[classes.input, classes.base].join(" ")}
+        ref={inputRef}
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            setEditMode(false);
+          }
+        }}
+      />
+    );
   }
 
   return (
-    <div className={classes.CellLabel} data-cell-id="1">
+    <div
+      className={[classes.label, classes.base].join(" ")}
+      data-cell-id="1"
+      onClick={enableEditMode}
+    >
       Value
     </div>
   );
