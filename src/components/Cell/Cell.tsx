cd160523a1408289653b2da6b9b385d0eaf07d65
Lucas Mallmann <lucasmallmann76@gmail.com>
2023-10-05 21:30:34 -0300

    wip: inserting nodes

--- src/components/Cell/Cell.tsx
+++ src/components/Cell/Cell.tsx
@@ -1,16 +1,28 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useRecoilState, useRecoilValue } from "recoil";
 import { useClickawayCell } from "@/hooks";
 import { cellState, evaluatedCellState } from "@/store/cell";
+import graphlib from "@dagrejs/graphlib";
 
 import classes from "./Cell.module.scss";
+import { dependencyGraphState } from "@/store/dependency";
+import { cellIdtoMatrixIndices } from "@/utils";
+// import { cellIdtoMatrixIndices } from "@/utils";
 
 type CellProps = {
   id: string;
 };
 
+cell = {
+  formula: "=A1",
+  value: "10",
+};
+
 function Cell({ id }: CellProps) {
   const [cellValue, setCellValue] = useRecoilState(cellState(id));
+
+  const [depGraph, setDepGraph] = useRecoilState(dependencyGraphState);
+
   const [isEditMode, setEditMode] = useState(false);
   const inputRef = useRef<HTMLInputElement>(null);
 
@@ -47,6 +59,39 @@ function Cell({ id }: CellProps) {
       }));
     }
   }
+
+  useEffect(() => {
+    if (!cellValue.evaluate || !cellValue.value.startsWith("=")) {
+      return;
+    }
+    // Verify the user input. if starts with '=' then it's a ref.
+    // Discover the cell id
+    const { row, column } = cellIdtoMatrixIndices(cellValue.value);
+
+    setDepGraph((graph) => {
+      graph.setNode(id);
+
+      const relationCellId = `${row}-${column}`;
+
+      graph.setNode(relationCellId);
+      graph.setEdge(id, relationCellId, {
+        from: id,
+        to: relationCellId,
+      });
+
+      // graph.no
+
+      if (!graphlib.alg.isAcyclic(graph)) {
+        // if there is a cycle
+        console.error("Graph is cyclical. Handle it.");
+      }
+
+      console.log("nodes ", graph.nodes());
+      console.log("edges ", graph.edges());
+
+      return graph;
+    });
+  }, [cellValue.evaluate, cellValue.value, id, setDepGraph]);
 
   if (isEditMode) {
     return (
--- src/store/cell.ts
+++ src/store/cell.ts
@@ -1,12 +1,13 @@
 import { atomFamily, selectorFamily } from "recoil";
 
-import { cellIdtoMatrixIndices, evaluateEquation } from "@/utils";
-
 export const cellState = atomFamily({
   key: "cellState",
-  default: {
-    value: "",
-    evaluate: false,
+  default: (id) => {
+    return {
+      id,
+      value: "",
+      evaluate: false,
+    };
   },
 });
 
@@ -19,41 +20,6 @@ export const evaluatedCellState = selectorFamily({
     ({ get }) => {
       try {
         const cell = get(cellState(id));
-
-        if (cell.value.startsWith("=")) {
-          const value = cell.value.slice(1); //A1
-          const cellName = value.match(/[A-Z]+[0-9]+/gi) || [];
-          const { row, column } = cellIdtoMatrixIndices(cellName[0] as string);
-          const referenceId = `${row}-${column}`;
-          const referenceCell = get(cellState(referenceId));
-          if (referenceCell.value.startsWith("=")) {
-            console.log("RODANDO DE NOVO");
-            const value = cell.value.slice(1); //A1
-            const cellName = value.match(/[A-Z]+[0-9]+/gi) || [];
-            const { row, column } = cellIdtoMatrixIndices(
-              cellName[0] as string
-            );
-            const referenceId = `${row}-${column}`;
-            console.log("REF CELL ID ", referenceId, value);
-            const referenceCell = get(cellState(referenceId));
-            return referenceCell.value;
-          } else {
-            return referenceCell.value;
-          }
-        }
-
-        // if (!cell.evaluate) {
-        //   return cell.value;
-        // }
-
-        // if (cell.value.startsWith("=")) {
-
-        //   console.log(
-        //     `cell ${id} is referencing ${newId} with value = ${referenceCell.value}`
-        //   );
-        //   return referenceCell.value;
-        // }
-
         return cell.value;
       } catch (error) {
         console.log(error);
