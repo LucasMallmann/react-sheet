b6aafdb8fe9712d784bf8728ba39b4fd9e17bd19
Lucas Mallmann <lucasmallmann76@gmail.com>
2023-10-06 23:19:21 -0300

    refactor: move styles to a separate folder

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import "./reset.css";
-import "./global.css";
+import "./style/reset.css";
+import "./style/global.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
