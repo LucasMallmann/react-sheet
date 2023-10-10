508c2c13025a73ddefc952a89ac02d834f9d9b05
Lucas Mallmann <lucasmallmann76@gmail.com>
2023-10-10 15:15:49 -0300

    test: ensure removeIdFromDependent works on success

---
+++ src/__tests__/utils/cell.test.ts
@@ -0,0 +1,32 @@
+import { describe } from "node:test";
+import { expect, it } from "vitest";
+
+import { removeIdFromDependents } from "@/utils/cell";
+
+describe("cell", () => {
+  describe("removeIdFromDependents", () => {
+    it("should remove the specified ID from dependents", () => {
+      const cells = {
+        cell1: { value: "1", formula: "", dependents: ["cell2", "cell3"] },
+        cell2: { value: "2", formula: "", dependents: ["cell1"] },
+        cell3: {
+          value: "3",
+          formula: "",
+          dependents: ["cell1", "cell2", "cell3"],
+        },
+        cell4: {
+          value: "3",
+          formula: "",
+          dependents: ["cell1", "cell2", "cell10"],
+        },
+      };
+
+      const updatedCells = removeIdFromDependents(cells, "cell1");
+
+      expect(updatedCells.cell1.dependents).toEqual(["cell2", "cell3"]);
+      expect(updatedCells.cell2.dependents).toEqual([]);
+      expect(updatedCells.cell3.dependents).toEqual(["cell2", "cell3"]);
+      expect(updatedCells.cell4.dependents).toEqual(["cell2", "cell10"]);
+    });
+  });
+});
