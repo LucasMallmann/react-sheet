4ec6553cb86a618088c9d432f93d57c1ac5e0a6b
Lucas Mallmann <lucasmallmann76@gmail.com>
2023-10-10 14:54:58 -0300

    test: ensure number-to-char works on success

---
+++ src/__tests__/utils/number-to-char.test.ts
@@ -0,0 +1,26 @@
+import { describe } from "node:test";
+import { expect, it } from "vitest";
+
+import { numberToChar } from "@/utils/number-to-char";
+
+describe("numberToChar", () => {
+  it('should return "A" for 1', () => {
+    const result = numberToChar(1);
+    expect(result).toBe("A");
+  });
+
+  it('should return "Z" for 26', () => {
+    const result = numberToChar(26);
+    expect(result).toBe("Z");
+  });
+
+  it('should return "AA" for 27', () => {
+    const result = numberToChar(27);
+    expect(result).toBe("AA");
+  });
+
+  it('should return "AB" for 28', () => {
+    const result = numberToChar(30);
+    expect(result).toBe("AD");
+  });
+});
